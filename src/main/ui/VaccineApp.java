ba8d38af1957ea2dd11c867705d71d9c1ada7557
Paul Yi <65927391+paulthetutor@users.noreply.github.com>
2021-07-26 17:58:33 -0700

    Completed (as best can do) Phase 1 - Task 5

--- src/main/ui/VaccineApp.java
+++ src/main/ui/VaccineApp.java
@@ -8,5 +8,64 @@ public class VaccineApp {
 
     }
 
-    //MO
+    //MODIFIES: this
+    //EFFECTS: processes user input
+    private void runVaccineHelper() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes user command
+    private void processCommand() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: initializes vaccine profiles
+    private void initVaccineProfiles() {
+
+    }
+
+
+    //EFFECTS: displays menu of options to user (create profile, select profile, delete profile,...
+    // //.. add vaccine, remove, edit vaccine)
+    private void displayMenu() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes creation of new vaccine profile
+    private void doCreateProfile() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: shows list of vaccine in selected vaccine profile
+    private void displayVaccines() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: deletes vaccine profile
+    private void deleteVaccineProfile() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes adding of vaccine to profile
+    private void addVaccineToProfile() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes removal of vaccine to profile
+    private void removeVaccineFromProfile() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: edits vaccine from profile
+    private void editVaccineFromProfile() {
+
+    }
 }
